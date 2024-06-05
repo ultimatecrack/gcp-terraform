0d7d5a0ca548ba42512d625dea4a01f6db1bc861
rakesh-panigrahy-ascent <panigrahyrakesh143@gmail.com>
2024-06-05 11:16:54 +0530

    initial code

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "tt-github-demo-bucket"
+  project                  = "tt-dev-001"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
