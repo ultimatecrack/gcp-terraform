ed39b8b5c3fd3baf5c7811f389a956af5c457dd9
rakesh-panigrahy-ascent <panigrahyrakesh143@gmail.com>
2024-06-05 23:51:07 +0530

    initial code

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "tt-github-demo-bucket"
-  project                  = "My First Project"
+  project                  = "graphic-adapter-425011-f4"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
